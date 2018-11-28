b5e604901377c8af7a6cd033ee18465d20cc22ca
xarou94 <xaris.anag@gmail.com>
2018-11-28 23:48:07 +0200

    toggle list completed without interactivity

---
+++ ArProject/Assets/ObjectScrollList.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite icon;
+    public bool state;
+}
+
+public class ObjectScrollList : MonoBehaviour {
+
+
+    public List<Item> itemlList;
+    // den einai aparetito 
+    public Transform contentPanel;
+    public ObjectScrollList otherObjectList; //se periptwsi pou xriastw allh lista
+    public ObjectPool toggleObjectPool;
+    
+	// Use this for initialization
+	void Start () {
+
+        RefreshDisplay();
+	}
+	
+	public void RefreshDisplay()
+    {
+        AddToggle();
+    }
+    private void AddToggle()
+    {
+        for ( int i=0; i < itemlList.Count; i++)
+        {
+            Item item = itemlList[i];
+            GameObject newToggle = toggleObjectPool.GetObject();
+            newToggle.transform.SetParent(contentPanel);
+
+            ToggleScript toggleScript = newToggle.GetComponent<ToggleScript>();
+            toggleScript.Setup(item, this);
+        }
+    }
+}
---
+++ ArProject/Assets/ToggleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleScript : MonoBehaviour {
+
+    public Toggle toggle;
+    public Text nameLabel;
+    public Image iconImage;
+
+    private Item item;
+    private ObjectScrollList scrollList;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Setup(Item currentItem, ObjectScrollList currentScrollList)
+    {
+        item = currentItem;
+        nameLabel.text = item.itemName;
+        iconImage.sprite = item.icon;
+
+        scrollList = currentScrollList;
+    }
+	
+
+}
