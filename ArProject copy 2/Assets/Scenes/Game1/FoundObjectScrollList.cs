3902d1ab636c9a2b613d57488f2a85abd7536fcc
xarou94 <xaris.anag@gmail.com>
2019-03-01 15:27:42 +0200

    side panel v1

---
+++ ArProject copy 2/Assets/Scenes/Game1/FoundObjectScrollList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FoundObjectScrollList : MonoBehaviour {
+
+    public Text foundlabel;
+    public Image iconImage;
+    public Text namelabel;
+
+
+}
